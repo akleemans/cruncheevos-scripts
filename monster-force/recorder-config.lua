cf77e26f58ff5c3f3f07bb11667c0fef74aae468
Adrianus Kleemans <a.kleemans@gmail.com>
2026-07-12 22:09:56 +0200

    Monster Force: Run sync command

--- monster-force/recorder-config.lua
+++ monster-force/recorder-config.lua
@@ -2,7 +2,7 @@
 -- `description`) before each recording session; the rest is per-game.
 return {
     -- this recording session
-    name = "cemetery-shadow-game-over",
+    name = "village2-teleporting-2-times-and-finish",
     description = "",
 
     -- this game
